e7855eb729d0a7a1956fe886b2f2e3e0126aac9f
Patricia Prado <patcprado@gmail.com>
2024-05-23 16:29:26 -0300

    Correção do header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import Button from "./Button";
 
-const Header = ({ title, onAdd, showAdd }) => {git pu
+const Header = ({ title, onAdd, showAdd }) => {
   const location = useLocation();
   return (
     <header className="header">
